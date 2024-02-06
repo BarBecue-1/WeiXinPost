233b85c5032ebc2131186e1d7a99ac951e780b3e
BarBecue-1 <73008561+BarBecue-1@users.noreply.github.com>
2024-02-06 19:23:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx1ccc535408962418"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "932a079aba9740717b2237eca6a6c4c4"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "u-5bXxZ4e2BkE070B7q_ei1mzh_tN3nveG3_S2ocYbg"
 # 课程消息,上课提醒
-template_id2 = "loe1yHWr************************************"
+#template_id2 = "loe1yHWr************************************"
 # 晚安心语
-template_id3 = "-fun9-2************************************"
+#template_id3 = "-fun9-2************************************"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["o_bw06aD8lPzjcHOxmcUHTx99RHo"]
 
 # 信息配置
 # 所在省份
@@ -81,32 +81,9 @@ classes = \
     }
 
 # 模板 1：每日提醒模板
-# 本周是开学的第: {{weeks.DATA}} 周
 # 今天是: {{date.DATA}}
 # 城市： {{city.DATA}}
 # 天气： {{weather.DATA}}
 # 最低气温: {{min_temperature.DATA}}
 # 最高气温: {{max_temperature.DATA}}
 # 今天是破壳日的第: {{love_day.DATA}} 天
-# 距离开学还有: {{birthday.DATA}} 天
-# ----------------今日课程----------------
-# 第一讲: {{firstClass.DATA}}
-# 第二讲: {{secondClass.DATA}}
-# 第三讲: {{thirdClass.DATA}}
-# 第四讲: {{fourthClass.DATA}}
-# 第五讲: {{fifthClass.DATA}}
-# 第六讲: {{sixthClass.DATA}}
-
-# 模板 2 课程单推
-# 课程信息: {{classInfo.DATA}}
-
-# 模板 3 晚安心语推送和第二天课程推送
-# {{goodNight.DATA}}
-# ----------------明日课程----------------
-# 明天是: {{week.DATA}}
-# 第一讲: {{firstClass.DATA}}
-# 第二讲: {{secondClass.DATA}}
-# 第三讲: {{thirdClass.DATA}}
-# 第四讲: {{fourthClass.DATA}}
-# 第五讲: {{fifthClass.DATA}}
-# 第六讲: {{sixthClass.DATA}}
